0abbdf62503e8aa55686ecee2c2217c539c01569
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-02-20 18:52:33 -0500

    Initial Commit

---
+++ Installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make Main Folder
+mkdir /Users/$USER/"The Lyons' Den Media"
+
+# Make subfolders for products
+mkdir /Users/$USER/"The Lyons' Den Media"/"Product Name Goes Here"
+
+# Make text document
+echo "This is the manual for x.x product
+enjoy" > /Users/$USER/"The Lyons' Den Media"/"Manual.txt"
